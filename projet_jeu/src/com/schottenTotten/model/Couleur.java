e8641c8d95363823a050822683a832a38d549b8d
STITE Amr <Amr.Stite@bordeaux-inp.fr>
2025-12-01 15:12:44 +0100

    Add Couleur.java

--- projet_jeu/src/com/schottenTotten/model/Couleur.java
+++ projet_jeu/src/com/schottenTotten/model/Couleur.java
@@ -1,5 +1,32 @@
 package com.schottenTotten.model;
 
 public class Couleur {
-
+	private int r;
+	private int g;
+	private int b;
+	private String name;
+	
+    public Couleur(String name, int r, int g, int b) {
+        this.name = name;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    public String getName() {
+    	return name;
+    }
+    public int getR() {
+    	return r;
+    }
+    public int getG(){
+    	return g;
+    }
+    public int getB() {
+    	return b;
+    }
+    
+    @Override
+    public String toString() {
+        return name + "(" + r + "," + g + "," + b + ")";
+    }
 }
