be6dcace30c322499c431bbd17dba8f524dcd320
antoine.husser <antoine.husser@enseirb-matmeca.fr>
2025-12-21 15:36:11 +0100

    evaluateurcombinaison et resultatcombinaison

---
+++ projet_jeu/src/com/schottenTotten/model/ResulatCombinaison.java
@@ -0,0 +1,67 @@
+package com.schottenTotten.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ResultCombination implements Comparable<ResultCombination> {
+
+    private final TypeCombinaison type;
+    private final int sum;
+
+    private final List<ClanCarte> cards;
+
+    public ResultCombination(TypeCombinaison type, int sum, List<ClanCarte> cards) {
+        this.type = type;
+        this.sum = sum;
+        this.cards = cards;
+    }
+
+    public TypeCombinaison getType() {
+        return type;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public List<ClanCarte> getCards() {
+        return cards;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ResultCombination other = (ResultCombination) o;
+
+        return sum == other.sum && Objects.equals(type, other.type) && Objects.equals(cards, other.cards);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, sum, cards);
+    }
+
+    
+    @Override
+    public int compareTo(ResultCombination other) {
+        int diffType = this.type.compareTo(other.type);
+        if (diffType != 0) {
+            return diffType;
+        }
+
+        if (this.sum < other.sum) {
+            return -1;
+        }
+        if (this.sum > other.sum) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "ResultCombination{type=" + type + ", sum=" + sum + ", cards=" + cards + '}';
+    }
+}
